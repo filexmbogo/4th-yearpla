8001d9ee03c76a63b6618456e53680bb201493c1
kamau filex <kamau@kali.home.co>
2024-10-11 18:06:27 +0300

    DOCERIZED

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port =process.env.port||10000
 
 //database
 const mongoose=require("mongoose")
-const url='mongodb://127.0.0.1/docs'
+const url='mongodb://mongodb:27017/docs'
 const conn=mongoose.createConnection(url)
 
 //gridfs to upload and serve files in chunks
